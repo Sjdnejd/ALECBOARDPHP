82cbdfde5652a1e610e18b466844e6d79b04d623
ALECBOARD <113574419+Sjdnejd@users.noreply.github.com>
2022-12-11 21:35:09 +0900

    Add files via upload

---
+++ theme/default/popup.php
@@ -0,0 +1,16 @@
+<?php
+	if(!defined('__AFOX__')) exit();
+
+?>
+
+<div class="container">
+
+	<div class="bs-popup-body">
+		<section>
+			<article>
+			<?php displayModule()?>
+			</article>
+		</section>
+	</div>
+
+</div>
